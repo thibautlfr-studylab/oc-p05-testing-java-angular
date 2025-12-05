834a631b40b6f78b71e32754658bbc5b1190825e
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-12-05 18:06:40 +0100

    tests: add end-to-end tests for logout functionality to validate successful logout and UI state changes

---
+++ front/cypress/e2e/logout.cy.ts
@@ -0,0 +1,33 @@
+describe('Logout spec', () => {
+  const user = {
+    id: 1,
+    username: 'user',
+    firstName: 'User',
+    lastName: 'User',
+    admin: false
+  };
+
+  it('Logout successfull', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      body: user
+    })
+
+    cy.intercept('GET', '/api/session', []).as('sessions')
+
+    cy.get('input[formControlName=email]').type("user@studio.com")
+    cy.get('input[formControlName=password]').type("test!1234")
+    cy.get('button[type=submit]').click()
+
+    cy.url().should('include', '/sessions')
+    
+    cy.get('span').contains('Logout').click()
+    
+    cy.url().should('include', '/')
+    
+    cy.get('span').contains('Login').should('be.visible')
+    cy.get('span').contains('Register').should('be.visible')
+    cy.get('span').contains('Logout').should('not.exist')
+  })
+});
