ad55d8660c79d11f9a04e7847aaa5c62d9d53e67
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-12-05 17:49:25 +0100

    tests: add end-to-end tests for login functionality to validate failed login, and button state

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -20,8 +20,33 @@ describe('Login spec', () => {
       []).as('session')
 
     cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    cy.get('input[formControlName=password]').type("test!1234")
+    cy.get('button[type=submit]').click()
 
     cy.url().should('include', '/sessions')
+  })
+
+  it('Login failed', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401,
+      body: {},
+    })
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type("test!1234")
+    cy.get('button[type=submit]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('Login button disabled when form invalid', () => {
+    cy.visit('/login')
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    // Missing password
+
+    cy.get('button[type=submit]').should('be.disabled')
   })
 });
